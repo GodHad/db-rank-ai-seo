06eba7aed4e1185c09052c86b712483f92cde021
GodHad <sunharius@gmail.com>
2024-11-08 21:47:36 -0800

    app changed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,47 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <title>DB Rank AI</title>
+    <meta name="google-site-verification" content="Apib7-x98H0j5cPqHWwSMm6dNU4GmODRoqxLiDzdx9I">
+        <meta name="octolytics-url" content="https://collector.github.com/github/collect"/>
+        <meta name="octolytics-actor-id" content="155072724"/>
+        <meta name="octolytics-actor-login" content="GodHad"/>
+        <meta name="octolytics-actor-hash" content="429f477890b208ea832eb207284f6c8dd52f97adcbc32f2ebc1b6e148d832017"/>
+        <meta name="analytics-location" content="/&lt;user-name&gt;" data-turbo-transient="true"/>
+        <meta name="user-login" content="GodHad">
+        <link rel="sudo-modal" href="/sessions/sudo_modal">
+        <meta name="viewport" content="width=device-width">
+        <meta name="description" content="😊I ❤ React👍. GodHad has 40 repositories available. Follow their code on GitHub.">
+        <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="GitHub">
+        <link rel="fluid-icon" href="https://github.com/fluidicon.png" title="GitHub">
+        <meta property="fb:app_id" content="1401488693436528">
+        <meta name="apple-itunes-app" content="app-id=1477376905, app-argument=https://github.com/GodHad"/>
+        <meta name="twitter:image" content="https://avatars.githubusercontent.com/u/155072724?v=4?s=400"/>
+        <meta name="twitter:site" content="@github"/>
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:title" content="GodHad - Overview"/>
+        <meta name="twitter:description" content="😊I ❤ React👍. GodHad has 40 repositories available. Follow their code on GitHub."/>
+        <meta property="og:image" content="https://avatars.githubusercontent.com/u/155072724?v=4?s=400"/>
+        <meta property="og:image:alt" content="😊I ❤ React👍. GodHad has 40 repositories available. Follow their code on GitHub."/>
+        <meta property="og:site_name" content="GitHub"/>
+        <meta property="og:type" content="profile"/>
+        <meta property="og:title" content="GodHad - Overview"/>
+        <meta property="og:url" content="https://github.com/GodHad"/>
+        <meta property="og:description" content="😊I ❤ React👍. GodHad has 40 repositories available. Follow their code on GitHub."/>
+        <meta property="profile:username" content="GodHad"/>
+        <link rel="shared-web-socket" href="wss://alive.github.com/_sockets/u/155072724/ws?session=eyJ2IjoiVjMiLCJ1IjoxNTUwNzI3MjQsInMiOjE1MTAyMDE4NDMsImMiOjM3NzE1NTUyODgsInQiOjE3MzExMzA5MjZ9--f10e42ee274104e4a4b21c20e77841e07d718c9a21717c22ef9025a2a51da557" data-refresh-url="/_alive" data-session-id="3345027bd4fcf155e4a5b9892baf6646afd861ed9e69d22c6b92fb03fde555b1">
+        <link rel="shared-web-socket-src" href="/assets-cdn/worker/socket-worker-1a9b1a7a6108.js">
+        <meta name="hostname" content="github.com">
+        <meta name="keyboard-shortcuts-preference" content="all">
+        <meta name="hovercards-preference" content="true">
+        <meta name="announcement-preference-hovercard" content="true">
+        <meta name="expected-hostname" content="github.com">
+        <meta http-equiv="x-pjax-version" content="cdfb9eea3a1eeda7f97401ba26bd2234c092d6121e138d803c0b3af542ba3d64" data-turbo-track="reload">
+        <meta http-equiv="x-pjax-csp-version" content="ace39c3b6632770952207593607e6e0be0db363435a8b877b1f96abe6430f345" data-turbo-track="reload">
+        <meta http-equiv="x-pjax-css-version" content="815fa3a03d2216dbeb8924c579cb11e76914329413c2c84dc0814edfa70a2cc8" data-turbo-track="reload">
+        <meta http-equiv="x-pjax-js-version" content="e48ba441c05f150e8c8d06f0d622f91486e2676675f236ae6206fc3499dbee1e" data-turbo-track="reload">
+        <meta name="turbo-cache-control" content="no-preview" data-turbo-transient="">
+        <meta name="octolytics-dimension-user_id" content="155072724"/>
+        <meta name="octolytics-dimension-user_login" content="GodHad"/>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
