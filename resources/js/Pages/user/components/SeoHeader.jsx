d9e65e43dce3803e2772ca1ff003c18f047f5397
GodHad <sunharius@gmail.com>
2024-11-08 22:56:24 -0800

    commit seo

--- resources/js/Pages/user/components/SeoHeader.jsx
+++ resources/js/Pages/user/components/SeoHeader.jsx
@@ -3,6 +3,7 @@ import { APP_URL } from "../../../variables/statics"
 import { Helmet } from "react-helmet"
 
 export default ({ content, title }) => {
+    console.log(content, title)
     return (
         <>
             <Helmet>
@@ -10,26 +11,14 @@ export default ({ content, title }) => {
             </Helmet>
             <InertiaHead>
                 {typeof window !== 'undefined' && <meta property="og:url" content="https://github.com/GodHad"/>}
-                {content && (
-                    <>
-                        {content.meta_title && (
-                            <>
-                                <meta name="twitter:title" content={content.meta_title} />
-                                <meta property="og:title" content={content.meta_title} />
-                            </>
-                        )}
-                        {content.meta_description && (
-                            <>
-                                <meta name="description" content={content.meta_description} />
-                                <meta name="twitter:description" content={content.meta_description} />
-                                <meta property="og:description" content={content.meta_description} />
-                                <meta property="og:image:alt" content={content.meta_description} />
-                            </>
-                        )}
-                        {content.twitter_graph_image && <meta name="twitter:image" content={`${APP_URL}storage/${content.twitter_graph_image}`} />}
-                        {content.og_graph_image && <meta property="og:image" content={`${APP_URL}storage/${content.og_graph_image}`} />}
-                    </>
-                )}
+                {content && content.meta_title && <meta name="twitter:title" content={content.meta_title} />}
+                {content && content.meta_title && <meta property="og:title" content={content.meta_title} />}
+                {content && content.meta_description && <meta name="description" content={content.meta_description} />}
+                {content && content.meta_description && <meta name="twitter:description" content={content.meta_description} />}
+                {content && content.meta_description && <meta property="og:description" content={content.meta_description} />}
+                {content && content.meta_description && <meta property="og:image:alt" content={content.meta_description} />}
+                {content && content.twitter_graph_image && <meta name="twitter:image" content={`${APP_URL}storage/${content.twitter_graph_image}`} />}
+                {content && content.og_graph_image && <meta property="og:image" content={`${APP_URL}storage/${content.og_graph_image}`} />}
             </InertiaHead>
         </>
     )
