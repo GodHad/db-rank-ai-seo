5e7faba112eccd29c9a6059aa11772394c1a470a
GodHad <sunharius@gmail.com>
2024-11-09 04:10:11 -0800

    update the order of meta link

--- resources/js/Pages/user/components/SeoHeader.jsx
+++ resources/js/Pages/user/components/SeoHeader.jsx
@@ -10,15 +10,15 @@ export default ({ content, title }) => {
                 <title>DB Rank AI | {title}</title>
             </Helmet>
             <InertiaHead>
-                {typeof window !== 'undefined' && <meta property="og:url" content={window.location.href}/>}
-                {content && content.meta_title && <meta name="twitter:title" content={content.meta_title} />}
-                {content && content.meta_title && <meta property="og:title" content={content.meta_title} />}
                 {content && content.meta_description && <meta name="description" content={content.meta_description} />}
+                {content && content.meta_title && <meta name="twitter:title" content={content.meta_title} />}
                 {content && content.meta_description && <meta name="twitter:description" content={content.meta_description} />}
+                {content && content.twitter_graph_image && <meta name="twitter:image" content={`${APP_URL}storage/${content.twitter_graph_image}`} />}
+                {content && content.meta_title && <meta property="og:title" content={content.meta_title} />}
                 {content && content.meta_description && <meta property="og:description" content={content.meta_description} />}
                 {content && content.meta_description && <meta property="og:image:alt" content={content.meta_description} />}
-                {content && content.twitter_graph_image && <meta name="twitter:image" content={`${APP_URL}storage/${content.twitter_graph_image}`} />}
                 {content && content.og_graph_image && <meta property="og:image" content={`${APP_URL}storage/${content.og_graph_image}`} />}
+                {typeof window !== 'undefined' && <meta property="og:url" content={window.location.href}/>}
             </InertiaHead>
         </>
     )
