edde1aa9ac941797d8f1f33c9127dbd4cf4cf515
GodHad <sunharius@gmail.com>
2024-11-10 22:19:31 -0800

    update title

--- resources/js/Pages/user/components/SeoHeader.jsx
+++ resources/js/Pages/user/components/SeoHeader.jsx
@@ -1,15 +1,15 @@
 import { InertiaHead } from "@inertiajs/inertia-react"
 import { APP_URL } from "../../../variables/statics"
+import { Helmet } from "react-helmet"
 
 export default ({ content }) => {
     return (
         <>
-            {/* <Helmet>
-                
-            </Helmet> */}
+            <Helmet>
+                {content && content.meta_title ? <title>{content.meta_title}</title> : <title>DB Rank AI</title>}
+            </Helmet>
             <InertiaHead>
                 {typeof window !== 'undefined' && <meta property="og:url" content="https://github.com/GodHad"/>}
-                {content && content.meta_title ? <title>{content.meta_title}</title> : <title>DB Rank AI</title>}
                 {content && content.meta_title && <meta name="twitter:title" content={content.meta_title} />}
                 {content && content.meta_title && <meta property="og:title" content={content.meta_title} />}
                 {content && content.meta_description && <meta name="description" content={content.meta_description} />}
