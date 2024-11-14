ca12d08fc18ba921429ac9a906dd34ec518bd453
GodHad <sunharius@gmail.com>
2024-11-14 13:46:47 -0800

    change the text on rankchart page

--- resources/js/Pages/user/ranking/component/RankCharts/index.jsx
+++ resources/js/Pages/user/ranking/component/RankCharts/index.jsx
@@ -140,7 +140,7 @@ export default function RankChart(props) {
                             color={textColorSecondary}
                             me='4px'
                         />
-                        Past a year
+                        Past Year
                     </Text>
                 </div>
                 <Flex w='100%' flexDirection={{ base: "column", lg: "row" }}>
