3001b2d8ce3c2295d2542029d675ded6eee64bf3
GodHad <sunharius@gmail.com>
2024-11-21 22:45:21 -0800

    update email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -26,7 +26,8 @@ class ContactController extends Controller
         if ($validator->fails()) return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
 
         Mail::send('emails.contact', $data, function ($message) use ($data) {
-            $message->from('dbrank.ai');
+            $message->from('office@dbrank.ai', 'Contact Form'); // Verified email
+            $message->replyTo($data['email']); // User's email
             $message->to('sunharius@gmail.com')
                     ->subject('Contact Request from ' . $data['firstname']);
         });
