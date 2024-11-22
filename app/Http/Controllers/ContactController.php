2ef5f5403928f886b387e2ed91a21a097d957d78
GodHad <sunharius@gmail.com>
2024-11-22 14:50:16 -0800

    update to office@dbrank.ai

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -28,7 +28,7 @@ class ContactController extends Controller
         Mail::send('emails.contact', $data, function ($message) use ($data) {
             $message->from('office@dbrank.ai', $data['firstname'] . ' ' . $data['lastname']); 
             $message->replyTo($data['email']);
-            $message->to('success.together0303@gmail.com')
+            $message->to('office@dbrank.ai')
                     ->subject('Contact Request from ' . $data['firstname']);
         });
         
