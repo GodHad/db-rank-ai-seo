8f1a22dace4bc19f85ce4d205e17b712a70f881e
GodHad <sunharius@gmail.com>
2024-11-22 22:28:32 -0800

    update the title

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -26,7 +26,7 @@ class ContactController extends Controller
         if ($validator->fails()) return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
 
         Mail::send('emails.contact', $data, function ($message) use ($data) {
-            $message->from('office@dbrank.ai', $data['firstname'] . ' ' . $data['lastname']); 
+            $message->from('office@dbrank.ai', $data['firstname'] . ' ' . $data['lastname'] . ' via DB Rank AI'); 
             $message->replyTo($data['email']);
             $message->to('success.together0303@gmail.com')
                     ->subject('Contact Request from ' . $data['firstname']);
